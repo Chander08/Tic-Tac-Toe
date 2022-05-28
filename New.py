194fd330c842899283aae46c9a89263ee14fba21
NavpreetSyan <navvsyan@gmail.com>
2022-05-28 17:03:03 -0400

    Update New.py

--- New.py
+++ New.py
@@ -9,12 +9,18 @@ pg.init()
 WIDTH = 600
 HEIGHT = 600
 # rbg = red green blue
-BG_COLOUR = (255, 255, 255)
+BG_COLOUR = (28, 170, 156)
 BLACK = (0,0,0)
 LINE_COLOUR = (23, 145, 135)
 LINE_WIDTH = 10
 BOARD_ROWS = 3
 BOARD_COLS = 3
+RED = (255, 0 , 0)
+CIRCLE_RADIUS = 60
+CIRCLE_WIDTH = 15
+CIRCLE_COLOUR = (239, 231, 200)
+CROSS_WIDTH = 10
+SPACE = 55
 
 screen = pg.display.set_mode( (WIDTH, HEIGHT))
 pg.display.set_caption( 'TIC-TAC-TOE')
@@ -34,6 +40,16 @@ def draw_line():
     pg.draw.line(screen, LINE_COLOUR, (400,0), (400,600), LINE_WIDTH)
 draw_line()
 
+def draw_figures():
+    for row in range(BOARD_ROWS):
+        for col in range(BOARD_COLS):
+            if board[row][col] == 1:
+                pg.draw.circle( screen, CIRCLE_COLOUR, (int( col * 200 + 100 ), int( row * 200 + 100)), CIRCLE_RADIUS, CIRCLE_WIDTH) 
+            elif board[row][col] == 2:
+                pg.draw.line( screen, BLACK, (col * 200 + SPACE, row * 200 + 200 - SPACE), (col * 200 + 200 - SPACE, row * 200 + SPACE), 30 )
+                pg.draw.line( screen, BLACK, (col * 200 + SPACE, row * 200 + SPACE), (col * 200 + 200 - SPACE, row * 200 + 200 - SPACE), 30)
+player = 1
+
 def mark_square(row, col, player):
     board[row][col]= player
 
@@ -46,8 +62,6 @@ def is_board_full():
             if board[row][col] == 0:
                 return False
     return True
-
-
 
 
 while True:
@@ -63,7 +77,7 @@ while True:
             clicked_row = int(mouseY // 200)
             clicked_col = int(mouseX // 200)
 
-            if available_square ( clicked_row, clicked_row):
+            if available_square ( clicked_row, clicked_col):
                 if player == 1:
                     mark_square( clicked_row, clicked_col, 1)
                     player = 2
@@ -72,7 +86,7 @@ while True:
                     mark_square(clicked_row, clicked_col, 2)
                     player = 1
 
-                
+                draw_figures()
 
             
  
